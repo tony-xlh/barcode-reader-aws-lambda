f0949ceb2d7f402600ad090cac1fd81ed553720f
xulihang <xulihanghai@163.com>
2023-08-29 15:10:49 +0800

    use input to get the endpoint

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import base64
 import json
 import requests
 
+endpoint = "" #like https://*****.execute-api.us-east-2.amazonaws.com/default/BarcodeReader
+
 def get_picture_base64_data(image_path):
     with open(image_path, 'rb') as image_file:
         base64_data = base64.b64encode(image_file.read())
@@ -12,9 +14,15 @@ def decode():
     body = {"base64": base64}
     json_data = json.dumps(body)
     headers = {'Content-type': 'application/json'}
-    r = requests.post('https://toln285pca.execute-api.us-east-2.amazonaws.com/default/BarcodeReader', data=json_data, headers=headers)
+    r = requests.post(endpoint, data=json_data, headers=headers)
     print(r.json())
 
+def get_endpoint_from_cli():
+    print("Please input the endpoint:")
+    global endpoint
+    endpoint = input()
 
 if __name__ == "__main__":
+    if endpoint == "":
+        get_endpoint_from_cli()
     decode()
