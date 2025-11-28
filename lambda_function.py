3fcde9d9a35cbf02a913cf26a710ca52634aedd5
xulihang <xulihanghai@163.com>
2025-11-28 11:26:40 +0800

    add v11 version

--- lambda_function.py
+++ lambda_function.py
@@ -1,51 +1,53 @@
 import json
 import base64
-from dbr import *
+from dynamsoft_barcode_reader_bundle import *
+
+
+DynamsoftLicense = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==" #one-day public trial
 
 folder_path = "/tmp/dynamsoft"
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
-# Sets a directory path for saving the license cache.
-BarcodeReader.set_license_cache_path(folder_path)
-error = BarcodeReader.init_license("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==") #one-day public trial
+
+LicenseManager.set_license_cache_path(folder_path)
+errorCode, errorMsg = LicenseManager.init_license(DynamsoftLicense)
 
 def lambda_handler(event, context):
-    try:
-        if error[0] != EnumErrorCode.DBR_OK:
-            return ("License error: "+ error[1])
+    if errorCode != EnumErrorCode.EC_OK and errorCode != EnumErrorCode.EC_LICENSE_CACHE_USED:
+        return ("License error: "+ errorMsg)
 
-        dbr = BarcodeReader()
-        request_body = event["body"]
-        base64_string = json.loads(request_body)["base64"]
+    cvr_instance = CaptureVisionRouter()
+    request_body = event["body"]
+    base64_string = json.loads(request_body)["base64"]
+    image_bytes = base64.b64decode(base64_string)
+    result = cvr_instance.capture(image_bytes, EnumPresetTemplate.PT_READ_BARCODES.value)
+    print(result)
+    barcode_result = result.get_decoded_barcodes_result()
+    result_dict = {}
+    results = []
 
-        text_results = dbr.decode_base64_string(base64_string)
-        result_dict = {}
-        results = []
+    if barcode_result != None:
+        for br in barcode_result.get_items():
+            result = {}
+            result["barcodeFormat"] = br.get_format_string()
+            result["barcodeText"] = br.get_text()
+            result["barcodeBytes"] = str(base64.b64encode(br.get_bytes()))[2:-1]
+            result["confidence"] = br.get_confidence()
+            quad = br.get_location()
+            points = quad.points
+            result["x1"] = points[0].x
+            result["y1"] = points[0].y
+            result["x2"] = points[1].x
+            result["y2"] = points[1].y
+            result["x3"] = points[2].x
+            result["y3"] = points[2].y
+            result["x4"] = points[3].x
+            result["y4"] = points[3].y
+            results.append(result)
+    result_dict["results"] = results
+    return {
+        "statusCode": 200,
+        "body": json.dumps(result_dict)
+    }
 
-        if text_results != None:
-            for tr in text_results:
-                result = {}
-                result["barcodeFormat"] = tr.barcode_format_string
-                result["barcodeText"] = tr.barcode_text
-                result["barcodeBytes"] = str(base64.b64encode(tr.barcode_bytes))[2:-1]
-                result["confidence"] = tr.extended_results[0].confidence
-                points = tr.localization_result.localization_points
-                result["x1"] = points[0][0]
-                result["y1"] = points[0][1]
-                result["x2"] = points[1][0]
-                result["y2"] = points[1][1]
-                result["x3"] = points[2][0]
-                result["y3"] = points[2][1]
-                result["x4"] = points[3][0]
-                result["y4"] = points[3][1]
-                results.append(result)
-        result_dict["results"] = results
-        return {
-            "statusCode": 200,
-            "body": json.dumps(result_dict)
-        }
-        return 
-    except BarcodeReaderError as bre:
-        print(bre)
-        return bre.error_info
         
---
+++ lambda_function_v9.py
@@ -0,0 +1,51 @@
+import json
+import base64
+from dbr import *
+
+folder_path = "/tmp/dynamsoft"
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+# Sets a directory path for saving the license cache.
+BarcodeReader.set_license_cache_path(folder_path)
+error = BarcodeReader.init_license("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==") #one-day public trial
+
+def lambda_handler(event, context):
+    try:
+        if error[0] != EnumErrorCode.DBR_OK:
+            return ("License error: "+ error[1])
+
+        dbr = BarcodeReader()
+        request_body = event["body"]
+        base64_string = json.loads(request_body)["base64"]
+
+        text_results = dbr.decode_base64_string(base64_string)
+        result_dict = {}
+        results = []
+
+        if text_results != None:
+            for tr in text_results:
+                result = {}
+                result["barcodeFormat"] = tr.barcode_format_string
+                result["barcodeText"] = tr.barcode_text
+                result["barcodeBytes"] = str(base64.b64encode(tr.barcode_bytes))[2:-1]
+                result["confidence"] = tr.extended_results[0].confidence
+                points = tr.localization_result.localization_points
+                result["x1"] = points[0][0]
+                result["y1"] = points[0][1]
+                result["x2"] = points[1][0]
+                result["y2"] = points[1][1]
+                result["x3"] = points[2][0]
+                result["y3"] = points[2][1]
+                result["x4"] = points[3][0]
+                result["y4"] = points[3][1]
+                results.append(result)
+        result_dict["results"] = results
+        return {
+            "statusCode": 200,
+            "body": json.dumps(result_dict)
+        }
+        return 
+    except BarcodeReaderError as bre:
+        print(bre)
+        return bre.error_info
+        
